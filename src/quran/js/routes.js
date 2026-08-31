a71a74a3bbba4ad401efc138271527e1c1cd609f
aviyansah-kly <avi.yansah@kly.id>
2026-08-31 16:17:07 +0700

    Align Quran route helpers with release runtime

--- src/quran/js/routes.js
+++ src/quran/js/routes.js
@@ -1,5 +1,5 @@
 window.QuranRoutes=Object.freeze({
-  home:()=>'/prototype-v6',
-  surah:(surah,ayat)=>ayat?`/surah-v6?surah=${surah}&ayat=${ayat}`:`/surah-v6?surah=${surah}`,
-  ayat:(surah,ayat)=>`/ayat-v6?surah=${surah}&ayat=${ayat}`
+  home:()=>'/prototype-v6.html',
+  surah:(surah,ayat)=>ayat?`/surah-v4.html?surah=${surah}&ayat=${ayat}`:`/surah-v4.html?surah=${surah}`,
+  ayat:(surah,ayat)=>`/ayat-v5.html?surah=${surah}&ayat=${ayat}`
 });
